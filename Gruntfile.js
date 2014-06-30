b3b296965e7804d81476a479c5af679edd1f5dde
Richard Bevers <richard.bevers@gmail.com>
2014-06-30 21:40:50 +0200

    Added docs, which still need work. Solved a minor conflict after merge

--- Gruntfile.js
+++ Gruntfile.js
@@ -154,7 +154,7 @@ module.exports = function(grunt) {
     grunt.registerTask(
         'build',
         'Compiles all of the assets and copies the files to the build directory.',
-        [ 'clean:build', 'copy', 'stylesheets', 'scripts' ]
+        [ 'clean:build', 'copy', 'stylesheets', 'scripts', 'yuidoc' ]
     );
 
     grunt.registerTask(
---
+++ docs/api.js
@@ -0,0 +1,18 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Gp.Validators.length",
+        "Gp.Validators.regexp",
+        "Gp.Validators.required"
+    ],
+    "modules": [
+        "Validator"
+    ],
+    "allModules": [
+        {
+            "displayName": "Validator",
+            "name": "Validator"
+        }
+    ]
+} };
+});
